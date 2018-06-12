a73301be2b68f19123aebd54b73ac51be423ed3c
Dmitriy <dmitriy@Dmitriys-iMac.local>
2018-06-13 06:48:56 +0800

    live mode update

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
-const port = 8000;
+const basicAuth = require('express-basic-auth');
+ 
+app.use(basicAuth({
+    users: { 'admin': 'supersecret' }
+}));
+const port = 24000;
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./app/routes')(app, {});
 app.listen(port, () => {
